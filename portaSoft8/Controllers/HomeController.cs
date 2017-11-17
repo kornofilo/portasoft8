0ff712e7f1198d203d56fa973247ecbfcfbf004c
Ricardo Rubio <ricardo.rubio@utp.ac.pa>
2017-11-16 20:28:52 -0500

    test commit

--- portaSoft8/Controllers/HomeController.cs
+++ portaSoft8/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace portaSoft8.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Esta es la pagina principal con inormacion del Portafolio.";
 
             return View();
         }
